823d9acddc95b40f0e223c2abfefb4a48f95760e
Sanyam <sanyamk30@gmail.com>
2021-07-14 16:43:01 +0530

    Simple Data displaying app

--- App.tsx
+++ App.tsx
@@ -1,115 +1,124 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Alert,
+  SafeAreaView,
+} from 'react-native';
+import axios from 'axios';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+interface dataPoint {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+const URL = 'https://jsonplaceholder.typicode.com/users';
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+const App = () => {
+  const [data, setData] = useState<dataPoint[]>([]);
+  const [renderedData, setRenderedData] = useState<dataPoint[]>([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(5);
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+  const appendNext = () => {
+    let tempData: dataPoint[] = renderedData;
+    for (let i = offset; i < offset + limit; i++) {
+      if (!data[i]) return;
+      //Alert.alert(JSON.stringify(data[i]));
+      tempData.push(data[i]);
+    }
+    setOffset(offset + limit);
+    setRenderedData(tempData);
+    Alert.alert(JSON.stringify(renderedData.length));
+  };
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+  const renderHeader = () => {
+    return <Text style={styles.header}>Details of Users</Text>;
+  };
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+  const randomGender = () => {
+    let num: number = Math.floor(Math.random() * 2);
+    return num == 0 ? 'Male' : 'Female';
+  };
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+  const randomAge = () => {
+    return Math.floor(Math.random() * 60) + 20;
+  };
 
- export default App;
+  useEffect(() => {
+    axios
+      .get<dataPoint[]>(URL)
+      .then(response => {
+        let tempData: dataPoint[] = [...response.data];
+        for (let i = 10; i < 50; i++) {
+          let copy = {...response.data[i % 10]};
+          copy.id = i + 1;
+          tempData.push(copy);
+        }
+        setData(tempData);
+      })
+      .catch(error => Alert.alert(error));
+
+    appendNext();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={renderedData}
+        onEndReached={appendNext}
+        onEndReachedThreshold={0.1}
+        renderItem={({item}) => (
+          <View style={styles.infoCard}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.phone}>Ph. Num - {item.phone}</Text>
+            <Text style={styles.address}>
+              Address - {item.address.suite} {item.address.street}{' '}
+              {item.address.city}
+            </Text>
+            <Text>Gender : {randomGender()}</Text>
+            <Text>Age : {randomAge()}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+        ListHeaderComponent={renderHeader}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  infoCard: {borderWidth: 2, padding: 20},
+  name: {fontSize: 20},
+  phone: {fontStyle: 'italic'},
+  address: {fontFamily: 'sans-serif', fontStyle: 'italic'},
+  header: {
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+});
+
+export default App;
